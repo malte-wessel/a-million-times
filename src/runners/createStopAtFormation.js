f35ab59006dff86e7abeefc5ab8bc70d894a7775
Malte Wessel <>
2017-11-04 11:38:20 +0100

    improve stop at formation

--- src/runners/createStopAtFormation.js
+++ src/runners/createStopAtFormation.js
@@ -1,34 +1,43 @@
-import distance from '../util/distance';
+import normalize from '../util/normalize';
 import result from '../util/result';
+
+const DECCELERATE_AFTER = 0.2;
+const STOP_AFTER = 0.001;
+const FACTOR = 0.5;
+
+const deccelerate = (value, to, v) => {
+    if (value === to) return 0;
+    let d = normalize(value < to ? to - value : value - to);
+
+    if (value < to && v < 0 || value > to && v > 0) {
+        d = 1 - d;
+    }
+
+    if (d < STOP_AFTER) {
+        return to - value;
+    }
+
+    if (d < DECCELERATE_AFTER) {
+        const c = v / (d / v) * FACTOR;
+        return v > 0 ? v - c : v + c;
+    }
+
+    return v;
+};
 
 export default function createStopAtFormation(createFormation) {
     return function init() {
         const formation = result(createFormation);
         return function update(x, y, index, value1, value2, v1, v2) {
-            const to1 = formation[index];
-            const to2 = formation[index + 1];
-
-            const d1 = v1 < 0 ? 1 - distance(value1, to1) : distance(value1, to1);
-            const d2 = v2 < 0 ? 1 - distance(value2, to2) : distance(value2, to2);
-
-            if (d1 > 0.001) {
-                if (d1 < 0.3) {
-                    v1 -= (v1 / (d1 * 2 / v1)) * (v1 < 0 ? -1 : 1);
-                }
-            } else {
-                v1 = 0;
-            }
-            if (d2 > 0.001) {
-                if (d2 < 0.3) {
-                    v2 -= (v2 / (d2 * 2 / v2)) * (v2 < 0 ? -1 : 1);
-                }
-            }  else {
-                v2 = 0;
-            }
-
             if (v1 === 0 && v2 === 0) {
                 return false;
             }
+
+            const to1 = formation[index];
+            const to2 = formation[index + 1];
+
+            v1 = deccelerate(value1, to1, v1);
+            v2 = deccelerate(value2, to2, v2);
 
             return [v1, v2];
         };
